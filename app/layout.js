902e07bd0b7edb5fd586fd2e2c67555ecbfc3a61
Prabesh Rijal <wsditdep@gmail.com>
2025-01-11 07:03:40 +0700

    all

--- app/layout.js
+++ app/layout.js
@@ -4,10 +4,8 @@ import "./styles/appLayout.scss";
 import "./styles/styles.scss";
 import "./styles/global.scss";
 import { Toaster } from "react-hot-toast";
-import { Suspense } from 'react'
 import LiveSupport from '@/components/liveSupport/LiveSupport';
 import favicon from "@/public/favicon.jpg";
-import Loader from '@/components/loading/Loader';
 
 export const metadata = {
   title: "Welcome - Travelopro",
@@ -34,13 +32,11 @@ export default function RootLayout({ children }) {
             },
           }}
         />
-        <Suspense fallback={<Loader />}>
           <div className="platform-layout-parent">
             <div className="platform-layout-childs">
               {children}
             </div>
           </div>
-        </Suspense>
         {/* <LiveSupport /> */}
       </body>
     </html>
